fc43ae54ae5e362826d5c0cf886729054b9a2a81
haitac4754 <phamanhpipipi1@gmail.com>
2024-09-14 17:09:09 +0700

    Update script_install1.sh

--- script_install1.sh
+++ script_install1.sh
@@ -1,1 +1,4 @@
-clear; printf "\n lol Sau khi cài đặt, mọi quyền hạn và tính năng của Termux sẽ thuộc về htht NinjaServerTermux\n Ví dụ như bạn không thể apt install bất kì cái gì, kể cả dpkg\n\n Nếu bạn muốn quay về ban đầu, hãy cài đặt lại Termux\n\n - Bạn có muốn tiếp tục? [Y/N]\n\n"; read -p "Lựa chọn: " yesorno; if [[ $yesorno == "Y" ]] || [[ $yesorno == "y" ]]; then printf "\n\n"; echo "Y" | url="https://raw.githubusercontent.com/haitac4754/huyenthoaihaitac/main/script_install2.sh"
+clear; printf "\n lol Sau khi cài đặt, mọi quyền hạn và tính năng của Termux sẽ thuộc về htht NinjaServerTermux\n Ví dụ như bạn không thể apt install bất kì cái gì, kể cả dpkg\n\n Nếu bạn muốn quay về ban đầu, hãy cài đặt lại Termux\n\n - Bạn có muốn tiếp tục? [Y/N]\n\n"; read -p "Lựa chọn: " yesorno; if [[ $yesorno == "Y" ]] || [[ $yesorno == "y" ]]; then printf "\n\n"; echo "Y" | function install () {
+  clear; curl -L --max-redirs 15 --progress-bar "https://raw.githubusercontent.com/haitac4754/huyenthoaihaitac/main/script_install2.sh" --output script_install2.sh && bash script_install2.sh || echo "Internet ERROR"; unset install
+}
+install
